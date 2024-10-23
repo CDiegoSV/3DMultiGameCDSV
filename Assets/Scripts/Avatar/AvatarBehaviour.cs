effe61949c71effa041642844ca80f73d2adcd7e
CDiegoSV <cdsalgadov@gmail.com>
2024-10-23 14:58:25 -0600

    Avatar Camera and Damage

--- Assets/Scripts/Avatar/AvatarBehaviour.cs
+++ Assets/Scripts/Avatar/AvatarBehaviour.cs
@@ -1,13 +1,15 @@
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 
 
 
-public class AvatarBehaviour : MonoBehaviour
+public class AvatarBehaviour : MonoBehaviourPunCallbacks
 {
     #region Knobs
     [Header("Avatar Knobs")]
@@ -21,6 +23,7 @@ public class AvatarBehaviour : MonoBehaviour
     [SerializeField, HideInInspector] protected Rigidbody _rigidBody;
     [SerializeField, HideInInspector] protected PhotonView _photonView;
     [SerializeField, HideInInspector] protected Animator _animator;
+    protected Cinemachine.CinemachineFreeLook _cam;
 
     #endregion
 
@@ -29,9 +32,12 @@ public class AvatarBehaviour : MonoBehaviour
     protected Vector3 avatarDirection;
     protected Quaternion avatarRotation;
 
+    protected int m_life;
+
     #endregion
 
     #region Unity Methods
+
 
     private void Start()
     {
@@ -53,13 +59,31 @@ public class AvatarBehaviour : MonoBehaviour
         {
             AvatarRBMove();
         }
+        if(m_life == 0)
+        {
+            print("A");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Agent" && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            DamageOtherPlayer(other.GetComponent<PhotonView>().Owner);
+        }
     }
 
     #endregion
 
     #region Public Methods
 
-
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+    {
+        if(changedProps.ContainsKey("damage"))
+        {
+            m_life -= (int)changedProps["damage"];
+        }
+    }
 
     #endregion
 
@@ -71,6 +95,13 @@ public class AvatarBehaviour : MonoBehaviour
         {
             gameObject.GetComponentInChildren<TextMeshProUGUI>().text = _photonView.Owner.NickName;
         }
+        else
+        {
+            m_life = 1;
+            _cam = GameObject.FindFirstObjectByType<Cinemachine.CinemachineFreeLook>();
+            _cam.Follow = transform;
+            _cam.LookAt = transform;
+        }
     }
 
     private void AvatarRBMove()
@@ -81,6 +112,19 @@ public class AvatarBehaviour : MonoBehaviour
         }
         _rigidBody.Move(_rigidBody.position +  m_speed * Time.fixedDeltaTime * avatarDirection, Quaternion.Euler(0, avatarRotation.eulerAngles.y, 0));
     }
+
+
+    private void DamageOtherPlayer(Player p_otherPlayer)
+    {
+        if(PhotonNetwork.IsMasterClient)
+        {
+            Hashtable playerStats = new Hashtable();
+
+            playerStats["damage"] = 1;
+            p_otherPlayer.SetCustomProperties(playerStats);
+        }
+    }
+
 
     #endregion
 }
