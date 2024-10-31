e992b20afdb3d0f4f4f20002512aea1e12e67025
CDiegoSV <cdsalgadov@gmail.com>
2024-10-31 11:55:44 -0600

    Update PlayerSpawnManager.cs

--- Assets/Scripts/Avatar/PlayerSpawnManager.cs
+++ Assets/Scripts/Avatar/PlayerSpawnManager.cs
@@ -29,6 +29,9 @@ public class PlayerSpawnManager : MonoBehaviour
         {
             instance = this;
         }
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Start()
